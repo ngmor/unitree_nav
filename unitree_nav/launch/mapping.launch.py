31b87595756433a56b92f8e4f69cffb989ed0bab
Nick Morales <ngmorales97@gmail.com>
2023-02-21 16:52:22 -0600

    Added robosense lidar launch file and rtabmap launch file

---
+++ unitree_nav/launch/mapping.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution([
+                    FindPackageShare('rslidar_sdk'),
+                    'launch',
+                    'start.py'
+                ])
+            ),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution([
+                    FindPackageShare('unitree_nav'),
+                    'launch',
+                    'rslidar_robosense.launch.py'
+                ])
+            ),
+        ),
+    ])
--- unitree_nav/launch/unitree_nav.launch.py
+++ unitree_nav/launch/unitree_nav.launch.py
@@ -2,7 +2,6 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch.conditions import IfCondition
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
